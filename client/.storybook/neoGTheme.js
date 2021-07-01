70a2a758bbb48771f35ffacda485f6c48ea21f89
sohamsshah <sohamshah456@gmail.com>
2021-07-01 19:38:13 +0530

    change theme

--- client/.storybook/neoGTheme.js
+++ client/.storybook/neoGTheme.js
@@ -3,7 +3,9 @@ import {
 } from '@storybook/theming'
 
 export default create({
-    base: 'light',
+    colorPrimary: '#00F0FF',
+    colorSecondary: '#737373',
+    base: 'dark',
     brandTitle: 'NeoG Platform93by4',
     brandUrl: 'https://github.com/neogcamp/platform-93by4',
     brandImage: 'https://pbs.twimg.com/profile_images/1350667071202332673/glWYSyvy.jpg',
