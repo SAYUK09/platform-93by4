e72f5e4138ee2ed2f5dd38548be9817ca247fa13
Sayuri Kamble <68416000+SAYUK09@users.noreply.github.com>
2021-07-02 22:02:47 +0530

    initial commit

--- client/src/pages/admin/review-portfolios/index.tsx
+++ client/src/pages/admin/review-portfolios/index.tsx
@@ -1,3 +1,12 @@
+import { Container } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
+
 export default function ReviewPortfolios(): JSX.Element {
-  return <div>Review Portfolios</div>
+  return (
+    <Container centerContent>
+      <Box padding="4" bg="gray.100" maxW="3xl">
+        REVIEW PORTFOLIO
+      </Box>
+    </Container>
+  )
 }
