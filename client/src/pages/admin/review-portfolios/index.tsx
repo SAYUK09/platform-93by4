1048a055df9df667dc39c4aa3a69660ada19f648
Sayuri Kamble <68416000+SAYUK09@users.noreply.github.com>
2021-07-03 13:23:51 +0530

    feat: button enabled only after text input

--- client/src/pages/admin/review-portfolios/index.tsx
+++ client/src/pages/admin/review-portfolios/index.tsx
@@ -15,95 +15,119 @@ import {
 } from '@chakra-ui/react'
 import { ChevronRightIcon } from '@chakra-ui/icons'
 import { Navbar } from '../../../components'
+import { useState } from 'react'
 
 export default function ReviewPortfolios(): JSX.Element {
+  const [disableButton, setDisableButton] = useState<boolean>(true)
+
+  function handleInputChange(e: { target: { value: string | number } }) {
+    if (e.target.value) {
+      setDisableButton(false)
+    } else {
+      setDisableButton(true)
+    }
+  }
+
   return (
     <div>
       <Navbar />
 
-      <Flex
-        backgroundColor="black.900"
-        align="center"
-        justify={{ base: 'center' }}
-        direction={{ base: 'column', md: 'column', sm: 'column' }}
-        minH="100vh"
-      >
-        <Container maxW="container.xl" width="90%">
-          <Box my="4" color="white" justifyContent="flex-start">
-            <Breadcrumb
-              spacing="8px"
-              separator={<ChevronRightIcon color="gray.500" />}
-            >
-              <BreadcrumbItem>
-                <BreadcrumbLink href="/">Dashboard</BreadcrumbLink>
-              </BreadcrumbItem>
-
-              <BreadcrumbItem>
-                <BreadcrumbLink href="/admin/review-portfolios">
-                  Review Portfolio
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-            </Breadcrumb>
-          </Box>
-
-          <Box>
-            <Heading color="brand.300" as="h3" size="xl">
-              Review Portfolio
-            </Heading>
-            <Box marginTop="4" color="white">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nisi
-              iure suscipit officiis. Neque consectetur perspiciatis labore
-              voluptatibus fugiat cumque, tenetur ipsa reiciendis, itaque
-              obcaecati corporis sequi corrupti, veritatis placeat?
-            </Box>
-
-            <Box px="8" paddingY="4" my="12" borderRadius="md" bg="black.800">
-              <Text
-                paddingY="2"
-                fontWeight="bold"
-                color="brand.300"
-                fontSize="sm"
+      <div style={{ minHeight: 'calc(100vh - 60px)' }}>
+        <Flex
+          backgroundColor="black.900"
+          align="center"
+          justify={{ base: 'center' }}
+          direction={{ base: 'column', md: 'column', sm: 'column' }}
+          minH="100%"
+        >
+          <Container maxW="container.xl" width="90%">
+            <Box my="4" color="black.400" justifyContent="flex-start">
+              <Breadcrumb
+                spacing="8px"
+                separator={<ChevronRightIcon color="gray.500" />}
               >
-                Application Number : #420
-              </Text>
-              <Text fontWeight="bold" color="white" fontSize="lg">
-                Name :{' '}
-                <span style={{ fontWeight: 'normal' }}>Tanay Pratap</span>
-              </Text>
+                <BreadcrumbItem>
+                  <BreadcrumbLink href="/">Dashboard</BreadcrumbLink>
+                </BreadcrumbItem>
 
-              <Text fontWeight="bold" color="white" fontSize="lg">
-                Portfolio Link :{' '}
-                <span style={{ fontWeight: 'normal' }}>
-                  https://tanay.netlify.app/
-                </span>
-              </Text>
+                <BreadcrumbItem>
+                  <BreadcrumbLink href="/admin/review-portfolios">
+                    Review Portfolio
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+              </Breadcrumb>
+            </Box>
 
-              <Box paddingY="6" paddingX="2">
-                <Textarea
-                  color="white"
-                  bg="black.600"
-                  placeholder="Please State Revisions Here"
-                />
+            <Box>
+              <Heading color="brand.300" as="h3" size="xl">
+                Review Portfolio
+              </Heading>
+              <Box marginTop="4" color="white">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
+                nisi iure suscipit officiis. Neque consectetur perspiciatis
+                labore voluptatibus fugiat cumque, tenetur ipsa reiciendis,
+                itaque obcaecati corporis sequi corrupti, veritatis placeat?
               </Box>
 
-              <Box padding="2" d="flex" mt="2" justifyContent="space-between">
-                <Button
-                  variant="outline"
-                  borderColor="brand.300"
-                  _hover={{ bg: 'brand.300', color: 'black' }}
+              <Box px="8" paddingY="4" my="12" borderRadius="md" bg="black.800">
+                <Text
+                  paddingY="2"
+                  fontWeight="bold"
                   color="brand.300"
-                  size="md"
+                  fontSize="sm"
                 >
-                  Button
-                </Button>
-                <Button variant="solid" bg="brand.300" size="md">
-                  Button
-                </Button>
+                  Application Number : #420
+                </Text>
+                <Text fontWeight="bold" color="white" fontSize="lg">
+                  Name :{' '}
+                  <span style={{ fontWeight: 'normal' }}>Tanay Pratap</span>
+                </Text>
+
+                <Text fontWeight="bold" color="white" fontSize="lg">
+                  Portfolio Link :{' '}
+                  <span style={{ fontWeight: 'normal' }}>
+                    https://tanay.netlify.app/
+                  </span>
+                </Text>
+
+                <Box paddingY="6" paddingX="2">
+                  <Textarea
+                    onChange={handleInputChange}
+                    border="black.800"
+                    color="white"
+                    bg="black.700"
+                    focusBorderColor="brand.300"
+                    placeholder="Please State Revisions Here"
+                  />
+                </Box>
+
+                <Box
+                  flexWrap="wrap"
+                  padding="2"
+                  d="flex"
+                  mt="2"
+                  justifyContent="space-between"
+                >
+                  <Button
+                    my="2"
+                    isDisabled={disableButton}
+                    variant="outline"
+                    borderColor="brand.300"
+                    _hover={{ bg: 'brand.300', color: 'black' }}
+                    color="brand.300"
+                    size="md"
+                  >
+                    Needs Revision
+                  </Button>
+                  <Button my="2" variant="solid" bg="brand.300" size="md">
+                    Ready For Mark15
+                  </Button>
+                </Box>
               </Box>
             </Box>
-          </Box>
-        </Container>
-      </Flex>
+          </Container>
+        </Flex>
+      </div>
     </div>
   )
 }
