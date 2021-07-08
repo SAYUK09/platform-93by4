bcaab7ab57aa95b531bda8bd9fa453a683462b2e
sushilburagute <codetastic101@gmail.com>
2021-07-08 18:07:58 +0530

    Add: Landing Page for the Project

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,24 +1,81 @@
-import { useState } from 'react'
-import { Text, Flex, Button } from '@chakra-ui/react'
-import { Layout, StatusCard, StepCard } from '../components'
-import {
-  data,
-  StatusType,
-  steps,
-  StepType,
-  submissionSting,
-} from '../data/staticData/admissionStages'
-import { theme } from '../themes'
+import { Box, Button, Heading, Stack, Text } from '@chakra-ui/react'
+import { Layout } from '../components'
+import illustration from '../../public/svgs/landingPage.svg'
+import Image from 'next/image'
+import { FiExternalLink } from 'react-icons/Fi'
+import { Footer } from '../components/Footer/Footer'
 
 export default function Home() {
-  const [currentStatus, setCurrentStatus] = useState('portfolio_not_submitted')
-  const status = data.find((e) => e.status == currentStatus)
-  console.log(status)
   return (
     <Layout>
-      <Flex as="section" flexDir="column">
-        <Text>Marketing page</Text>
-      </Flex>
+      <Stack
+        direction={{ base: 'column-reverse', lg: 'row' }}
+        spacing={{ base: '3rem', lg: '4rem' }}
+        align={{ lg: 'center' }}
+        justify="space-between"
+      >
+        <Box flex="1" maxW={{ lg: '520px' }}>
+          <Heading
+            as="h1"
+            size="3xl"
+            color={'white'}
+            fontWeight="extrabold"
+            letterSpacing="tighter"
+          >
+            Your learning journey starts here.
+          </Heading>
+          <Text color={'gray.400'} mt="4" fontSize="lg" fontWeight="medium">
+            Signup to start your admission process for neoG Camp (levelOne)
+          </Text>
+          <Stack
+            direction={{ base: 'column', md: 'row' }}
+            spacing="4"
+            mt={{ base: '12', md: '8' }}
+          >
+            <Button
+              size="lg"
+              minW="210px"
+              colorScheme="brand.500"
+              height="14"
+              px="8"
+              color="black.900"
+            >
+              Signup
+            </Button>
+            <Button
+              size="lg"
+              bg="black.900"
+              color="brand.500"
+              _hover={{ bg: 'black.800' }}
+              height="14"
+              px="8"
+              shadow="base"
+              border="2px"
+              borderColor="brand.500"
+            >
+              Login
+            </Button>
+          </Stack>
+          <Button
+            variant="link"
+            color="gray.400"
+            rightIcon={<FiExternalLink fontSize="xl" />}
+            size="md"
+            mt={{ base: '12', md: '8' }}
+          >
+            Know more about the camp
+          </Button>
+          <Footer />
+        </Box>
+        <Box
+          pos="relative"
+          w={{ base: 'full', lg: '500px' }}
+          h={{ base: 'auto', lg: '500px' }}
+          px={4}
+        >
+          <Image src={illustration} layout="responsive" />
+        </Box>
+      </Stack>
     </Layout>
   )
 }
