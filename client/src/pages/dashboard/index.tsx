b103aa3d7558642479e2c7ac5c615fb4a4c17c44
sushilburagute <codetastic101@gmail.com>
2021-07-08 09:25:16 +0530

    Fix: Route for Student Dashboard
    Shifted the Dashboard page from the home directory to it's very own page.

---
+++ client/src/pages/dashboard/index.tsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import { Text, Flex, Button } from '@chakra-ui/react'
+import { Layout, StatusCard, StepCard } from '../../components'
+import {
+  data,
+  StatusType,
+  steps,
+  StepType,
+  submissionSting,
+} from '../../data/staticData/admissionStages'
+import { theme } from '../../themes'
+
+export default function Dashboard() {
+  const [currentStatus, setCurrentStatus] = useState('portfolio_not_submitted')
+  const status = data.find((e) => e.status == currentStatus)
+  console.log(status)
+  return (
+    <Layout>
+      <Flex as="section" flexDir="column">
+        <StatusCard status={status} bgColor={theme.colors.black['800']} />
+        <Text
+          my={8}
+          color={theme.colors.gray['100']}
+          fontWeight="bold"
+          fontSize={['md', 'md', 'xl']}
+        >
+          <Button
+            onClick={() => setCurrentStatus('portfolio_under_review')}
+          ></Button>
+          <Button
+            onClick={() =>
+              setCurrentStatus('portfolio_passed_interview_to_be_scheduled')
+            }
+          ></Button>
+          <Button
+            onClick={() => setCurrentStatus('portfolio_needs_revision')}
+          ></Button>
+
+          {submissionSting}
+        </Text>
+
+        <Flex flexDir="column">
+          {steps.map((step: StepType, index: number) => {
+            return (
+              <StepCard
+                bgColor={theme.colors.black['800']}
+                step={step}
+                status={status as StatusType}
+                key={index.toString()}
+                index={index}
+              />
+            )
+          })}
+        </Flex>
+      </Flex>
+    </Layout>
+  )
+}
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -10,48 +10,14 @@ import {
 } from '../data/staticData/admissionStages'
 import { theme } from '../themes'
 
-export default function Dashboard() {
+export default function Home() {
   const [currentStatus, setCurrentStatus] = useState('portfolio_not_submitted')
   const status = data.find((e) => e.status == currentStatus)
   console.log(status)
   return (
     <Layout>
       <Flex as="section" flexDir="column">
-        <StatusCard status={status} bgColor={theme.colors.black['800']} />
-        <Text
-          my={8}
-          color={theme.colors.gray['100']}
-          fontWeight="bold"
-          fontSize={['md', 'md', 'xl']}
-        >
-          <Button
-            onClick={() => setCurrentStatus('portfolio_under_review')}
-          ></Button>
-          <Button
-            onClick={() =>
-              setCurrentStatus('portfolio_passed_interview_to_be_scheduled')
-            }
-          ></Button>
-          <Button
-            onClick={() => setCurrentStatus('portfolio_needs_revision')}
-          ></Button>
-
-          {submissionSting}
-        </Text>
-
-        <Flex flexDir="column">
-          {steps.map((step: StepType, index: number) => {
-            return (
-              <StepCard
-                bgColor={theme.colors.black['800']}
-                step={step}
-                status={status as StatusType}
-                key={index.toString()}
-                index={index}
-              />
-            )
-          })}
-        </Flex>
+        <Text>Marketing page</Text>
       </Flex>
     </Layout>
   )
