979d86126194e4cb15d0dcd4216f4dab258853fd
Nada Farook <nairah1619@gmail.com>
2021-07-09 12:24:05 +0530

    Adding dashboard route

--- src/routes/StudentRoutes.ts
+++ src/routes/StudentRoutes.ts
@@ -11,9 +11,6 @@ const router = Router()
 router.route('/submit').post(requiresAuth, submitHandler)
 router.route('/resubmit').post(requiresAuth, reSubmitHandler)
 
-<<<<<<< HEAD
 router.route('/dashbord-info').get(requiresAuth, dashboardInfoHandler)
-=======
->>>>>>> develop
 
 export = router
