59740a82d271f387801ebc25efc043fe7b65e666
Nada Farook <nairah1619@gmail.com>
2021-07-12 17:10:46 +0530

    Updated dashboard

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { Text, Flex, Button } from '@chakra-ui/react'
+import { useState, useEffect } from 'react'
+import { Text, Flex } from '@chakra-ui/react'
 import { Layout, StatusCard, StepCard } from '../../components'
 import {
   data,
@@ -9,34 +9,42 @@ import {
   submissionSting,
 } from '../../data/staticData/admissionStages'
 import { theme } from '../../themes'
+import { getDashboard } from '../../services/axiosService'
+
 import withAuth from '../../context/WithAuth'
 
 function Dashboard() {
   const [currentStatus, setCurrentStatus] = useState('portfolio_not_submitted')
+  const [submissionNo, setSubmissionNo] = useState(null)
+  useEffect(() => {
+    async function fetch() {
+      await getDashboard().then((user) => {
+        console.log(user)
+        const portfolio = user.foundPortfolio.portfolioUrl
+        portfolio !== undefined &&
+          (setCurrentStatus(portfolio.status),
+          setSubmissionNo(portfolio.submissionNo))
+      })
+    }
+    fetch()
+  }, [])
+
   const status = data.find((e) => e.status == currentStatus)
   console.log(status)
   return (
     <Layout>
       <Flex as="section" flexDir="column">
-        <StatusCard status={status} bgColor={theme.colors.black['800']} />
+        <StatusCard
+          status={status}
+          bgColor={theme.colors.black['800']}
+          submissionNo={submissionNo}
+        />
         <Text
           my={8}
           color={theme.colors.gray['100']}
           fontWeight="bold"
           fontSize={['md', 'md', 'xl']}
         >
-          <Button
-            onClick={() => setCurrentStatus('portfolio_under_review')}
-          ></Button>
-          <Button
-            onClick={() =>
-              setCurrentStatus('portfolio_passed_interview_to_be_scheduled')
-            }
-          ></Button>
-          <Button
-            onClick={() => setCurrentStatus('portfolio_needs_revision')}
-          ></Button>
-
           {submissionSting}
         </Text>
 
