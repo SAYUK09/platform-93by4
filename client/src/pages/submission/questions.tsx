58d007db28ae2edc804780cc3a833edaf575cab5
sohamsshah <sohamshah456@gmail.com>
2021-07-12 13:42:34 +0530

    minor fixes

--- client/src/pages/submission/questions.tsx
+++ client/src/pages/submission/questions.tsx
@@ -1,7 +1,6 @@
 import { Layout, Card, Breadcrumbs } from '../../components'
 import withAuth from '../../context/WithAuth'
 import { QuestionData } from '../../data/strings/questions'
-import { Breadcrumbs } from './../../components/BreadCrumbs/BreadCrumbs'
 
 export function QuestionsBeforeSubmission() {
   const breadcrumbsLinks = [
