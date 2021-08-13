6e822546f5b7d3822af53118df89e1a0ed3f6144
sushilburagute <codetastic101@gmail.com>
2021-08-13 12:12:38 +0530

    Fixed the issues which were breaking the build

--- client/src/data/strings/contact.ts
+++ client/src/data/strings/contact.ts
@@ -2,8 +2,6 @@ export const ContactString = {
   heading: 'Have any issue with the platform?',
   email: 'contact.neogcamp@gmail.com',
   contact: 'Contact us at: ',
-  paragraph: `It’s the <strong>BEST WAY </strong>to reach us quickly. <br />
-  Queries will be resolved within{' '}
-  <strong>3 - 5 working days.</strong> `,
+  paragraph: `It’s the BEST WAY to reach us quickly. Queries will be resolved within 3 - 5 working days.`,
   buttonText: 'Go back to dashboard',
 }
